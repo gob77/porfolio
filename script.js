ae6b7acafb166909601d80be53eca168fe32adae
gob77 <mraortiz@hotmail.com>
2016-10-24 12:04:27 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+        $(".button-collapse").sideNav({
+          edge: 'right',
+          menuWidth: 200
+        });
+        $('.parallax').parallax();
+        $('.modal-trigger').leanModal();
+    });
